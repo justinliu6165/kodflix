e0c562216a0a8e4bfbcb8648778078c1c9a4c658
Justin Liu <justin_liu@hotmail.co.uk>
2018-06-08 14:58:34 +0100

    change hello to goodbye

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>Helloo ricardo</p>
+        <p>Goodbye ricardo</p>
       </div>
     );
   }
