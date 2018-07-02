78878ff5df489985b5ea3fd6e6a545a2ec56684d
Justin Liu <justin_liu@hotmail.co.uk>
2018-07-02 15:59:06 +0100

    Replace TV show titles with covers #2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
 import React, { Component } from 'react';
 import './App.css';
+import BlackMirror from './Images/BlackMirror.jpg';
+import BreakingBad from './Images/BreakingBad.jpg';
+import DeathNote from './Images/DeathNote.jpg';
+import FearTheWalkingDead from './Images/FearTheWalkingDead.jpg';
+import GoT from './Images/GoT.jpg';
+import TheWire from './Images/TheWire.jpg';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
         <div className="container">
-          <h1 className="poster">Black Mirror</h1>
-          <h1 className="poster">Breaking Bad</h1>
-          <h1 className="poster">Death Note</h1>
+          <div className="item" >
+            <img src={BlackMirror} alt="Black Mirror"/>
+          </div>
+          <div className="item" >
+            <img src={BreakingBad} alt="Breaking Bad"/>
+          </div>
+          <div className="item" >
+            <img src={DeathNote} alt="Death Note"/>
+          </div>
         </div>
         <div className="container">
-          <h1 className="poster">Game of Thrones</h1>
-          <h1 className="poster">The Walking Dead</h1>
-          <h1 className="poster">The Wire</h1>
-        </div>
-
-        
+          <div className="item" >
+          <img src={GoT} alt="Game of Thrones"/>
+          </div>
+          <div className="item" >
+          <img src={FearTheWalkingDead} alt="Fear the Walking Dead"/>
+          </div>
+          <div className="item" >
+          <img src={TheWire} alt="The Wire"/>
+          </div>
+        </div>  
       </div>
     );
   }
