0182c91a91ce381e606668bef752544304132bde
Justin Liu <justin_liu@hotmail.co.uk>
2018-07-02 11:30:22 +0100

    Add movie title poster

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Inception from './Images/inception.jpg';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Kodflix</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>Goodbye ricardo</p>
+        
+        <img src={Inception} alt="Inception Poster" className="poster" />
+
+        
       </div>
     );
   }
