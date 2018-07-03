7ca434f9c6868e44256b4dfb7852febe6399d8f1
Justin Liu <justin_liu@hotmail.co.uk>
2018-07-03 09:35:53 +0100

    Componentize the TV show element #7

--- src/App.js
+++ src/App.js
@@ -7,37 +7,29 @@ import FearTheWalkingDead from './Images/FearTheWalkingDead.jpg';
 import GoT from './Images/GoT.jpg';
 import TheWire from './Images/TheWire.jpg';
 
+function MovieItem (props) {
+  return (
+    <div className="item">
+      <img src={props.src} alt={props.alt}/>
+      <div className="title"><h2>{props.alt}</h2></div>
+    </div>
+  )
+}
+
 class App extends Component {
+
   render() {
     return (
       <div className="App">
         <div className="container">
-          <div className="item" >
-            <img src={BlackMirror} alt="Black Mirror"/>
-            <div className="title"><h2>Black Mirror</h2></div>
-          </div>
-          <div className="item" >
-            <img src={BreakingBad} alt="Breaking Bad"/>
-            <div className="title"><h2>Breaking Bad</h2></div>
-          </div>
-          <div className="item" >
-            <img src={DeathNote} alt="Death Note"/>
-            <div className="title"><h2>Death Note</h2></div>
-          </div>
+          <MovieItem src={BlackMirror} alt='Black Mirror'/>
+          <MovieItem src={BreakingBad} alt='Breaking Bad'/>
+          <MovieItem src={DeathNote} alt='Death Note'/>
         </div>
         <div className="container">
-          <div className="item" >
-            <img src={GoT} alt="Game of Thrones"/>
-            <div className="title"><h2>Game Of Thrones</h2></div>
-          </div>
-          <div className="item" >
-            <img src={FearTheWalkingDead} alt="Fear the Walking Dead"/>
-            <div className="title"><h2>Fear The Walking Dead</h2></div>
-          </div>
-          <div className="item" >
-            <img src={TheWire} alt="The Wire"/>
-            <div className="title"><h2>The Wire</h2></div>
-          </div>
+          <MovieItem src={FearTheWalkingDead} alt='Fear The Walking Dead'/>
+          <MovieItem src={GoT} alt='Game Of Thrones'/>
+          <MovieItem src={TheWire} alt='The Wire'/>
         </div>  
       </div>
     );
