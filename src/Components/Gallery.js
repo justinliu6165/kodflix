ed15f4ef2005de25e0597c0b514ca4354eec6e97
Justin Liu <justin_liu@hotmail.co.uk>
2018-07-04 14:22:03 +0100

    Move the covers data model to a proper front-end data source #12

--- src/Components/Gallery.js
+++ src/Components/Gallery.js
@@ -1,25 +1,17 @@
-import BlackMirror from '../Images/BlackMirror.jpg';
-import BreakingBad from '../Images/BreakingBad.jpg';
-import DeathNote from '../Images/DeathNote.jpg';
-import FearTheWalkingDead from '../Images/FearTheWalkingDead.jpg';
-import GoT from '../Images/GoT.jpg';
-import TheWire from '../Images/TheWire.jpg';
 import MovieItem from './MovieItem';
 import React from 'react';
+import TVShows from './gallery-get';
+
 
 const Gallery = () => {
-    return ( 
-      <div>
-        <div className="container">
-          <MovieItem id='black-mirror' src={BlackMirror} alt='Black Mirror'/>
-          <MovieItem id='breaking-bad' src={BreakingBad} alt='Breaking Bad' />
-          <MovieItem id='death-note' src={DeathNote} alt='Death Note' />
-          <MovieItem id='fear-the-walking-dead' src={FearTheWalkingDead} alt='Fear The Walking Dead' />
-          <MovieItem id='game-of-thrones' src={GoT} alt='Game Of Thrones' />
-          <MovieItem id='the-wire' src={TheWire} alt='The Wire' />
-        </div>
+  return (
+    <div>
+      <div className="container">
+        {TVShows.map(show => <MovieItem id={show.id} src={show.src} alt={show.alt} />
+        )}
       </div>
-    )
-  };
+    </div>
+  )
+};
 
-  export default Gallery;
+export default Gallery;
---
+++ src/Components/gallery-get.js
@@ -0,0 +1,46 @@
+import BlackMirror from '../Images/BlackMirror.jpg';
+import BreakingBad from '../Images/BreakingBad.jpg';
+import DeathNote from '../Images/DeathNote.jpg';
+import FearTheWalkingDead from '../Images/FearTheWalkingDead.jpg';
+import GoT from '../Images/GoT.jpg';
+import TheWire from '../Images/TheWire.jpg';
+
+
+
+
+let TVShows = [
+    {
+        id: 'black-mirror',
+        src: BlackMirror,
+        alt: 'Black Mirror'
+    },
+    {
+        id: 'breaking-bad',
+        src: BreakingBad,
+        alt: 'Breaking Bad'
+    },
+    {
+        id: 'death-note',
+        src: DeathNote,
+        alt: 'Death Note'
+    },
+    {
+        id: 'fear-the-walking-dead',
+        src: FearTheWalkingDead,
+        alt: 'Fear The Walking Dead'
+    },
+    {
+        id: 'game-of-thrones',
+        src: GoT,
+        alt: 'Game Of Thrones'
+    },
+    {
+        id: 'the-wire',
+        src: TheWire,
+        alt: 'The Wire'
+    },
+]
+
+
+export default TVShows;
+
