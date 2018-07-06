a1b3ffad7a1dabf894dd162a93fba672247d7125
Justin Liu <justin_liu@hotmail.co.uk>
2018-07-06 14:06:40 +0100

    Return a list of shows from the backend #20

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
+const TvShows = require('./listOfShows')
 
+app.get('/', (req, res) => {
+    console.log('Hello World')
+})
 
-app.get('/', (req, res) => res.send("Hello World"));
-
-
-
+app.get('/rest/shows', (req, res) => {
+    res.send(TvShows);
+})
 
 
 app.listen(3000, ()=>console.log("Server is listening to port 3000!!!"));
