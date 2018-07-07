9ca6212b8440816729ff339a12e501dcf2543352
Justin Liu <justin_liu@hotmail.co.uk>
2018-07-07 11:05:10 +0100

    Improve setup of backend server #21

--- src/backend/app.js
+++ src/backend/app.js
@@ -11,4 +11,4 @@ app.get('/rest/shows', (req, res) => {
 })
 
 
-app.listen(3000, ()=>console.log("Server is listening to port 3000!!!"));
+app.listen(3001, ()=>console.log("Server is listening!!!"));
